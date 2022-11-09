65b9db93708fb6d5d953169876e2e823344d72c0
Win Htet Aung <winhtetaung@Wins-MacBook-Pro.local>
2022-11-09 17:46:57 +0630

    Data Pass Into Templates1

--- projects/views.py
+++ projects/views.py
@@ -2,8 +2,30 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+project_list = [
+    {'id':1,
+    'name':'vick',
+    'age':10,
+    'title':'ok'},
+    {'id':2,
+    'name':'rake',
+    'age':15,
+    'title':'nook'},
+    {'id':3,
+    'name':'kate',
+    'age':102,
+    'title':'okkk'}
+]
+
 def projects(request):
-    return render(request, 'projects.html')
+    msg = "Hello You are on message page"
+    num = 10
+    context = {'msg':msg,'age':num}
+    return render(request, 'projects/projects.html',{'projects':project_list})
 
 def project(request, pk):
-    return render(request, 'single-projects.html')
+    projectOBJ = None
+    for i in project_list:
+        if i['id'] == int(pk):
+            projectOBJ = i
+    return render(request, 'projects/single-projects.html',{'project':projectOBJ})

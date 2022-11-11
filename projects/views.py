ee130ea4306cea0714abb75a1f59a13e39c5575a
Win Htet Aung <winhtetaung@Wins-MacBook-Pro.local>
2022-11-11 16:28:09 +0630

    Model Forms

--- projects/views.py
+++ projects/views.py
@@ -2,21 +2,7 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 from .models import Project
-
-project_list = [
-    {'id':1,
-    'name':'vick',
-    'age':10,
-    'title':'ok'},
-    {'id':2,
-    'name':'rake',
-    'age':15,
-    'title':'nook'},
-    {'id':3,
-    'name':'kate',
-    'age':102,
-    'title':'okkk'}
-]
+from .forms import ProjectForm
 
 def projects(request):
     projects = Project.objects.all()
@@ -27,3 +13,8 @@ def project(request, pk):
     projectOBJ = Project.objects.get(id=pk)
     tags = projectOBJ.tags.all()
     return render(request, 'projects/single-projects.html',{'projectOBJ':projectOBJ,'tags':tags})
+
+def createProject(request):
+    form = ProjectForm()
+    context = {'form':form}
+    return render(request, "projects/project_form.html", context)

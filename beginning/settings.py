248eafac10468881149fb9ce44b906bd29fad952
Win Htet Aung <winhtetaung@Wins-MacBook-Pro.local>
2022-11-18 09:32:50 +0630

    signals

--- beginning/settings.py
+++ beginning/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'projects.apps.ProjectsConfig',
     'users.apps.UsersConfig',
+    'fontawesome_5',
+    'fontawesomefree'
 ]
 
 MIDDLEWARE = [
--- users/models.py
+++ users/models.py
@@ -3,10 +3,16 @@ from django.contrib.auth.models import User
 import uuid
 # Create your models here.
 
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=200, blank=True, null=True)
     email = models.EmailField(max_length=500, blank=True, null=True)
+    username = models.CharField(max_length=200, null=True, blank=True)
+    location = models.CharField(max_length=200, null=True, blank=True)
     short_intro = models.CharField(max_length=200, blank=True, null=True)
     bio = models.TextField(blank=True, null=True)
     profile_image = models.ImageField(null=True, blank=True, upload_to='profiles/', default='profiles/user-default.png')
@@ -17,8 +23,28 @@ class Profile(models.Model):
     social_instagram = models.CharField(max_length=200, blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    
+    # @property
+    # def imageURL(self):
+    #     try:
+    #         url = self.profile_image.url
+    #     except:
+    #         url = ''
+    #     return url
 
     def __str__(self):
-        return str(self.user.username)
+        return str(self.username)
+
+
+
+class Skill(models.Model):
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=200, blank=True, null=True)
+    description = models.TextField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.name)
 
 
